ab4d99bb46920ea3fa822ac4771499bf1bcb248f
pplenty <newtype26@naver.com>
2023-04-03 22:48:16 +0900

    섹션3: 내장 톰캣 + servlet 으로 띄우기

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,36 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @SpringBootApplication
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    PrintWriter writer = resp.getWriter();
+                    writer.println("Hello " + name);
+                }
+            }).addMapping("/hello");
+        });
+        webServer.start();
+    }
 }
